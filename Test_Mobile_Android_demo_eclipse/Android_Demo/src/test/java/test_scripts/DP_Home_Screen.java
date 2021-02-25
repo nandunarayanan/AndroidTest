a80208eac74d4e3e5575024cfad12f62436aac7a
nandunarayanan <67416440+nandunarayanan@users.noreply.github.com>
2021-02-25 10:37:22 +0530

    Add files via upload

--- Test_Mobile_Android_demo_eclipse/Android_Demo/src/test/java/test_scripts/DP_Home_Screen.java
+++ Test_Mobile_Android_demo_eclipse/Android_Demo/src/test/java/test_scripts/DP_Home_Screen.java
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * File Name 	: DP_Home_Screen.java
  * 
- * Versn		: 0.1
+ * Version		: 0.1
  * 
  * Author		: Bastin
  * 
@@ -14,35 +14,32 @@
  *****************************************************************************/
 package test_scripts;
 import org.testng.annotations.Test;
-
 /*Inclusion of required libraries*/
+import org.testng.asserts.Assertion;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
-
 import tcsLib.applicationLaunchLib;
-import tcsLib.dbUtils;
 
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
-import org.json.simple.JSONObject;
 import org.testng.annotations.AfterTest;
 import tcsLib.screenshotLib;
 import tcsLib.yamlLib;
 import utility.ExcelUtils;
 
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
+import static org.testng.Assert.assertEquals;
 
 import java.util.Map;
+
 /*Below class contains the test scripts for the Home screen*/
 /*TestNG settings for run all the defined test cases*/
 public class DP_Home_Screen  
 {
 	static int count_1 = 0;
 	public AppiumDriver<MobileElement> driver;
-	JSONObject document;
+	
 	/*Data provider with excel settings in the name of PatientData*/
 	@DataProvider
 	public Object[][] PatientData() 
@@ -50,8 +47,12 @@ public class DP_Home_Screen
 	  try
 		{
 		  	
-			ExcelUtils.setExcelFile("/home/318356/eclipse_snap_workspace/Android_Demo/src/test/java/test_data/Test.xlsx","Sheet3");			
-			Object[][] testObjArray = ExcelUtils.getTableArray("/home/318356/eclipse_snap_workspace/Android_Demo/src/test/java/test_data/Test.xlsx","Sheet3");		
+			ExcelUtils.setExcelFile("E:\\Eclipse\\Workspace\\"
+					+ "Test_DP_Mobile_App\\src\\test\\java\\test_data"
+					+ "\\Test.xlsx","Sheet3");			
+			Object[][] testObjArray = ExcelUtils.getTableArray("E:\\Eclipse"
+					+ "\\Workspace\\Test_DP_Mobile_App\\src\\test\\java\\"
+					+ "test_data\\Test.xlsx","Sheet3");		
 			return (testObjArray);	
 		}
 		
@@ -63,36 +64,116 @@ public class DP_Home_Screen
 		}
 		return null;
 	}	
+	
+	/*Data provider with excel settings in the name of PatientData*/
+	@DataProvider
+	public Object[][] PatientData1() 
+	{
+	  try
+		{
+		  	
+			ExcelUtils.setExcelFile("E:\\Eclipse\\Workspace\\"
+					+ "Test_DP_Mobile_App\\src\\test\\java\\test_data"
+					+ "\\Test.xlsx","Sheet1");			
+			Object[][] testObjArray = ExcelUtils.getTableArray("E:\\Eclipse"
+					+ "\\Workspace\\Test_DP_Mobile_App\\src\\test\\java\\"
+					+ "test_data\\Test.xlsx","Sheet1");		
+			return (testObjArray);	
+		}
+		
+		catch(Exception e)
+		{
+			System.out.println("Error Cause is:- "+e.getCause());
+			System.out.println("Error Message is:- "+e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 	/**************************************************************************
-	 * Test Name  : DP_Home_Screen_TC_3
+	 * Test Name  : DP_Home_Screen_TC_4
 	 * 
-	 * Test ID	  : TC_3
+	 * Test ID	  : TC_4
 	 * 
-	 * Type		  :(Parameterized/Data Driven/other) Data Driven
+	 * Type		  :(Parameterized/Data Driven/other) other
 	 * 
 	 * Group	  :(Smoke/Sanity/Regression) Regression
 	 * 
-	 * Description: This test case will input the patient data from the
-	 * 				excel and verify the screen functions are working 
+	 * Description: This test case will verify the screen functions are working 
 	 * 				as per the SRS document
 	 *************************************************************************/
-	  @Test(priority = 3, dataProvider = "PatientData", 
-			description = "Add Patient details screen testing "
-								+ "with dataProvider")
-	  public void DP_Home_Screen_TC_3(String sDOB, String sGender, 
-			  			String sFather_Name, String sMother_Name) 
-	  {
-		  
+
+	  @Test(priority = 2, description = "Add Patient screen testing "
+	  								+ "with parameterized testing",
+	  								enabled = false)  //dependsOnMethods = {"DP_Home_Screen_TC_4", "DP_Home_Screen_TC_5"}
+	  public void DP_Home_Screen_TC_5()
+    {
 		  count_1++;
-		  
+		 // MobileElement el5 = (MobileElement) driver.findElementById
+					//("us.drpad.drpadapp:id/lv_patient");
+		  //el5.click();
+		  MobileElement el15 = (MobileElement) driver.findElementById
+					("us.drpad.drpadapp:id/imgAddPatient");
+		  el15.click();
+		  MobileElement el26 = (MobileElement) driver.findElementById
+					("us.drpad.drpadapp:id/edt_first_name");
+		  el26.sendKeys("Test");
+		  MobileElement el27 = (MobileElement) driver.findElementById
+								("us.drpad.drpadapp:id/edt_lastname");		  
+		  String L_name = String.valueOf(count_1);
+		  el27.sendKeys(L_name);
+		
+		  MobileElement el8 = (MobileElement) driver.findElementById
+							("us.drpad.drpadapp:id/btn_done");
+		  el8.click();
+
+    }
+
+	/**************************************************************************
+	 * Test Name  : DP_Home_Screen_TC_4
+	 * 
+	 * Test ID	  : TC_4
+	 * 
+	 * Type		  :(Parameterized/Data Driven/other) other
+	 * 
+	 * Group	  :(Smoke/Sanity/Regression) Regression
+	 * 
+	 * Description: This test case will verify the screen functions are working 
+	 * 				as per the SRS document
+	 *************************************************************************/
+	  @Test(priority = 4, dataProvider = "PatientData1", description = "Home screen testing with priority and "
+  		+ "description facility", enabled = false)
+    public void DP_Home_Screen_TC_4(String sDOB, String sGender, 
+  						String sFather_Name, String sMother_Name,
+  						String sAddr, String sPh, String sAPh, String sEmail,
+  						String sBT, String sHT, String sWT, String sOccu,
+  						String sCom)
+    {
+		  count_1++;
+		  MobileElement el5 = (MobileElement) driver.findElementById
+					("us.drpad.drpadapp:id/lv_patient");
+		  el5.click();
+		  MobileElement el15 = (MobileElement) driver.findElementById
+	  				("us.drpad.drpadapp:id/imgAddPatient");
+		  el15.click();
+		  MobileElement el26 = (MobileElement) driver.findElementById
+					("us.drpad.drpadapp:id/edt_first_name");
+		  el26.sendKeys("Test");
+		  MobileElement el27 = (MobileElement) driver.findElementById
+								("us.drpad.drpadapp:id/edt_lastname");		  
+		  String L_name = String.valueOf(count_1);
+		  el27.sendKeys(L_name);
+		
+		  MobileElement el8 = (MobileElement) driver.findElementById
+							("us.drpad.drpadapp:id/btn_done");
+		  el8.click();
+
 		  MobileElement el1 = (MobileElement) driver.findElementById
 				  				("us.drpad.drpadapp:id/txt_title_age");
 		  el1.click();
-		  screenshotLib.captureScreenshot("DP_Home_Screen_TC_3", driver);
-		  MobileElement el2 = (MobileElement) driver.findElementById
+		  MobileElement el12 = (MobileElement) driver.findElementById
 				  				("us.drpad.drpadapp:id/edt_dob");
-		  el2.sendKeys(sDOB);
+		  el12.sendKeys(sDOB);
 		  System.out.println("sDOB: "+sDOB);
 		  if(sGender.equalsIgnoreCase("Male"))
 		  {
@@ -113,24 +194,164 @@ public class DP_Home_Screen
 		  MobileElement el20 = (MobileElement) driver.findElementById
 				  			("us.drpad.drpadapp:id/edt_mother_name");
 		  el20.sendKeys(sMother_Name);
+		  MobileElement el21 = (MobileElement) driver.findElementById
+					("us.drpad.drpadapp:id/btn_save");
+		  el21.click();
+
+		  System.out.println("Contains:..... "+ driver.getPageSource().contains(sFather_Name));
+		  assertEquals(true, driver.getPageSource().contains(sFather_Name));
 		  
-		  screenshotLib.captureScreenshot("DP_Home_Screen_TC_3_"
-				  				+ "b4_Complete", driver);
-		  if(count_1 == 4)
+		  MobileElement el01 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/txt_title_address");
+		  el01.click();
+		  MobileElement el02 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/edt_address");
+		  el02.click();
+		  el02.sendKeys(sAddr);
+		  MobileElement el113 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/edt_phone");
+		  el113.sendKeys(sPh);
+		  MobileElement el4 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/edt_alt_phone");
+		  el4.sendKeys(sAPh);
+		  MobileElement el115 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/edt_email");
+		  el115.sendKeys(sEmail);
+		  MobileElement el6 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/btn_save");
+		  el6.click();
+		  MobileElement el7 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/txt_title_motes");
+		  el7.click();
+		  MobileElement el18 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/edt_bloodtype");
+		  el18.sendKeys(sBT);
+		  MobileElement el9 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/edt_weight");
+		  el9.clear();
+		  MobileElement el10 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/edt_weight");
+		  el10.sendKeys(sWT);
+		  MobileElement el11 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/edt_hieght");
+		  el11.sendKeys(sHT);
+		  MobileElement el112 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/edt_company");
+		  el112.sendKeys(sCom);
+		  MobileElement el13 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/edt_occupation");
+		  el13.sendKeys(sOccu);
+		  driver.hideKeyboard();
+		  MobileElement el14 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/btn_save");
+		  el14.click();
+		  try
 		  {
-			  MobileElement el21 = (MobileElement) driver.findElementById
-					  					("us.drpad.drpadapp:id/btn_save");
-			  el21.click();
+			  System.out.println("sFather_Name: "+sFather_Name);
+			  System.out.println("Contains: "+ driver.getPageSource().contains(sFather_Name));
+
+			  screenshotLib.captureScreenshot("DP_Home_Screen_TC_4_"
+					  				+ "b4_Err", driver);
+			  assertEquals(true, driver.getPageSource().contains(sFather_Name));
+			  assertEquals(true, driver.getPageSource().contains(sMother_Name));
+			  assertEquals(true, driver.getPageSource().contains(sAddr));
+			  assertEquals(true, driver.getPageSource().contains(sPh));
+			  assertEquals(true, driver.getPageSource().contains(sAPh));
+			  assertEquals(true, driver.getPageSource().contains(sBT));
+			  assertEquals(true, driver.getPageSource().contains(sWT));
+			  assertEquals(true, driver.getPageSource().contains(sHT));
+			  assertEquals(true, driver.getPageSource().contains(sCom));
+			  assertEquals(true, driver.getPageSource().contains(sOccu));
+		  }
+		  catch(Exception e)
+			{
+				System.out.println("Error Cause is:- "+e.getCause());
+				System.out.println("Error Message is:- "+e.getMessage());
+				e.printStackTrace();
+			}
+		  MobileElement el04 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/ic_clinic");
+		  el04.click();
+		  el04.click();
+		  MobileElement el2 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/txt_home");
+		  el2.click();
+		  
+		//assertEquals();
+		  
+    }
+
+	/**************************************************************************
+	 * Test Name  : DP_Home_Screen_TC_3
+	 * 
+	 * Test ID	  : TC_3
+	 * 
+	 * Type		  :(Parameterized/Data Driven/other) Data Driven
+	 * 
+	 * Group	  :(Smoke/Sanity/Regression) Regression
+	 * 
+	 * Description: This test case will input the patient data from the
+	 * 				excel and verify the screen functions are working 
+	 * 				as per the SRS document
+	 *************************************************************************/
+	  @Test(priority = 3, dataProvider = "PatientData", 
+			description = "Add Patient details screen testing "
+								+ "with dataProvider", enabled = false)
+	  public void DP_Home_Screen_TC_3(String sDOB, String sGender, 
+			  			String sFather_Name, String sMother_Name) 
+	  {
+		  count_1++;
+		  MobileElement el5 = (MobileElement) driver.findElementById
+					("us.drpad.drpadapp:id/lv_patient");
+		  el5.click();
+		  MobileElement el15 = (MobileElement) driver.findElementById
+	  				("us.drpad.drpadapp:id/imgAddPatient");
+		  el15.click();
+
+		  MobileElement el126 = (MobileElement) driver.findElementById
+					("us.drpad.drpadapp:id/edt_first_name");
+		  el126.sendKeys("Test");
+		  MobileElement el127 = (MobileElement) driver.findElementById
+								("us.drpad.drpadapp:id/edt_lastname");
+		  String L_name = String.valueOf(count_1);
+		  el127.sendKeys(L_name);
+		
+		  MobileElement el118 = (MobileElement) driver.findElementById
+							("us.drpad.drpadapp:id/btn_done");
+		  el118.click();
+		  MobileElement el1 = (MobileElement) driver.findElementById
+				  				("us.drpad.drpadapp:id/txt_title_age");
+				  el1.click();
+
+		  MobileElement el112 = (MobileElement) driver.findElementById
+				  				("us.drpad.drpadapp:id/edt_dob");
+		  el112.sendKeys(sDOB);
+		  System.out.println("sDOB: "+sDOB);
+		  if(sGender.equalsIgnoreCase("Male"))
+		  {
+			  MobileElement el1118 = (MobileElement) driver.findElementById
+					  					("us.drpad.drpadapp:id/rd_male");
+			  el1118.click();
 		  }
 		  else
 		  {
-			  MobileElement el22 = (MobileElement) driver.findElementById
-					  					("us.drpad.drpadapp:id/btn_cancel");
-			  el22.click();
+			  MobileElement el19 = (MobileElement) driver.findElementById
+					  				("us.drpad.drpadapp:id/rd_female");
+			  el19.click();
 		  }
-		  screenshotLib.captureScreenshot("DP_Home_Screen_TC_3_"
-		  									+ "Complete", driver);
-	  
+
+		  MobileElement el19 = (MobileElement) driver.findElementById
+				  				("us.drpad.drpadapp:id/edt_father_name");
+		  el19.sendKeys(sFather_Name);
+		  MobileElement el20 = (MobileElement) driver.findElementById
+				  			("us.drpad.drpadapp:id/edt_mother_name");
+		  el20.sendKeys(sMother_Name);
+			  
+		  MobileElement el21 = (MobileElement) driver.findElementById
+				  					("us.drpad.drpadapp:id/btn_save");
+		  el21.click();
+		//  assertEquals(true, driver.getPageSource().contains(sDOB));
+		  try
+		  {
+			  assertEquals(true, driver.getPageSource().contains(sFather_Name));
+			  assertEquals(true, driver.getPageSource().contains(sMother_Name));
+		  }
+		  catch(Exception e)
+			{
+				System.out.println("Error Cause is:- "+e.getCause());
+				System.out.println("Error Message is:- "+e.getMessage());
+				e.printStackTrace();
+			}
+		  MobileElement el04 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/ic_clinic");
+		  el04.click();
+		  el04.click();
+		  MobileElement el2 = (MobileElement) driver.findElementById("us.drpad.drpadapp:id/txt_home");
+		  el2.click();
+			  
 	  }
 
 	/**************************************************************************
@@ -150,7 +371,7 @@ public class DP_Home_Screen
   @Parameters({"F_name","L_name"})
   @Test(priority = 2, description = "Add Patient screen testing "
   								+ "with parameterized testing",
-  				dependsOnMethods = {"DP_Home_Screen_TC_1"})
+  				dependsOnMethods = {"DP_Home_Screen_TC_1"}, enabled = false)
   public void DP_Home_Screen_TC_2(String F_name, String L_name) 
   {
 	  MobileElement el5 = (MobileElement) driver.findElementById
@@ -163,17 +384,22 @@ public class DP_Home_Screen
 			  						("us.drpad.drpadapp:id/edt_lastname");
 	  el27.sendKeys(L_name);
 
-	  screenshotLib.captureScreenshot("DP_Home_Screen_TC_2", driver);
 	  MobileElement el8 = (MobileElement) driver.findElementById
 			  					("us.drpad.drpadapp:id/btn_done");
 	  el8.click();
-	  screenshotLib.captureScreenshot("DP_Home_Screen_TC_2_Complete", driver);
+	  
+	  MobileElement el9 = (MobileElement) driver.findElementById
+							("us.drpad.drpadapp:id/ic_clinic");
+	  el9.click();
+	  
+	  assertEquals(true, driver.getPageSource().contains(F_name));
+	  assertEquals(true, driver.getPageSource().contains(L_name));
   }	
 
 /**************************************************************************
  * Test Name  : DP_Home_Screen_TC_1
  * 
- * Test ID	  : TC_2
+ * Test ID	  : TC_1
  * 
  * Type		  :(Parameterized/Data Driven/other) other
  * 
@@ -183,15 +409,12 @@ public class DP_Home_Screen
  * 				as per the SRS document
  *************************************************************************/
   @Test(priority = 1, description = "Home screen testing with priority and "
-  		+ "description facility")
+  		+ "description facility", enabled = false)
   public void DP_Home_Screen_TC_1() 
   {
 
-	/*  MobileElement el4 = (MobileElement) driver.findElementById
-			  			("us.drpad.drpadapp:id/lv_patient"); */
 	  MobileElement el4 = (MobileElement) driver.findElementById
-	  			((String) (dbUtils.dbGetElement(document, "LoginScreen", "Button" , "e14")));
-	  
+			  			("us.drpad.drpadapp:id/lv_patient");
 	  el4.click();
 	  MobileElement el5 = (MobileElement) driver.findElementById
 			  				("us.drpad.drpadapp:id/imgAddPatient");
@@ -202,12 +425,34 @@ public class DP_Home_Screen
 	  MobileElement el7 = (MobileElement) driver.findElementById
 			  				("us.drpad.drpadapp:id/edt_lastname");
 	  el7.sendKeys("01");
-	  screenshotLib.captureScreenshot("DP_Home_Screen_TC_1", driver);
 
 	  MobileElement el8 = (MobileElement) driver.findElementById
 			  					("us.drpad.drpadapp:id/btn_cancel");
 	  el8.click();
-	  screenshotLib.captureScreenshot("DP_Home_Screen_TC_1_Complete", driver);
+	  
+	  //Need to check the screen verification call
+	  assertEquals(true,true);
+  }
+  
+
+/**************************************************************************
+ * Test Name  : DP_Home_Screen_TC_0
+ * 
+ * Test ID	  : TC_0
+ * 
+ * Type		  :(Parameterized/Data Driven/other) other
+ * 
+ * Group	  :(Smoke/Sanity/Regression) Regression
+ * 
+ * Description: This test case will verify the screen functions are working 
+ * 				as per the SRS document
+ *************************************************************************/
+  @Test(priority = 0, description = "Home screen testing with priority and "
+  		+ "description facility",enabled = false)
+  public void DP_Home_Screen_TC0() 
+  {
+	  //Need to check the screen verification call
+	  assertEquals(true,true);
   }
   /* As per test NG this is the function which will call before the test
 	  *  automatically.*/
@@ -216,18 +461,11 @@ public class DP_Home_Screen
   {
 	  try
 		{
-		  DB db = dbUtils.dbGetDatabase("localhost", 27017, "MobileAppElementsRepo");
-		//  DBCollection collection = db.getCollection("App1");
-		  //dbUtils.InsertJsonData(collection, "/home/318356/eclipse_snap_workspace/Android_Demo/src/test/java/utility/test.json");
-		  document = dbUtils.dbGetDoument(db,"App1",1, "/home/318356/eclipse_snap_workspace/Android_Demo/src/test/java/utility/json_array_output.json");
-          String text = dbUtils.dbGetElement(document, "LoginScreen", "Button" , "Text");
-	      System.out.println("Text from method: "+text);
-		  
 		  Map<String, String> map;
-		  String path = "/home/318356/eclipse_snap_workspace/Android_Demo/src/test/java/utility/test.yaml";
+		  String path = "E:\\Eclipse\\Workspace\\Test_DP_Mobile_App\\src"
+		   + "\\test\\java\\utility\\test.yaml";
 		
 		  map = yamlLib.yamlRead(path);
-		  System.out.println("Path of the config"+map.get("path_config"));
 		  driver = applicationLaunchLib.appLaunch(map.get("path_config"),map.get("app_id"));		
 		}
 		catch(Exception e)
